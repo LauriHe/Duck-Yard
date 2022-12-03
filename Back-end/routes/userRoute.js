4701768f65b1fb8b1d35ee4eb0ad464a70769057
LauriHe <Lauri.Heikkinen5@metropolia.fi>
2022-12-03 23:53:25 +0200

    getUserLikes

--- Back-end/routes/userRoute.js
+++ Back-end/routes/userRoute.js
@@ -6,6 +6,7 @@ const {
   user_put,
   user_delete,
   check_token,
+  user_likes_get,
 } = require("../controllers/userController");
 const router = express.Router();
 
@@ -15,7 +16,9 @@ router.get("/token", check_token);
 
 router.route("/:id").get(user_get).delete(user_delete);
 
+router.route("/likes/:id").get(user_likes_get);
+
 module.exports = router;
 
 // Delete this one later
-// git hub 
+// git hub
