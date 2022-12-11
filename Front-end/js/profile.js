2c7b0f24819284179b2f3e7ba7b83fc0a614e766
sarao <saraok@metropolia.fi>
2022-12-11 13:42:22 +0200

    fixes to profile

--- Front-end/js/profile.js
+++ Front-end/js/profile.js
@@ -11,29 +11,33 @@ function createUser(user) {
     card.classList.add("profCard");
 
     const img = document.createElement("div");
-    img.classList.add("cardImage");
+    img.classList.add("profImg");
     const imgUrl = liveServerUrl + "/Back-end/uploads/" + profile.image;
     img.style.cssText += `background-image:url("${imgUrl}");`;
 
     // Lisää otsikko
+    const heading = document.createElement("h3");
+    name.innerHTML = `Omat tiedot:`;
+    name.classList.add("profHeading");
 
     const name = document.createElement("p");
     name.innerHTML = `Nimi: ${user.name}`;
-    name.classList.add("cardLocation");
+    name.classList.add("profName");
 
     const puh = document.createElement("p");
     puh.innerHTML = `Puhelinnumero: ${user.phone}`;
-    puh.classList.add("cardLocation");
+    puh.classList.add("profPuh");
 
     const email = document.createElement("p");
     email.innerHTML = `Sähköposti: ${user.email}`;
-    email.classList.add("cardLocation");
+    email.classList.add("profEmail");
 
     const area = document.createElement("p");
     area.innerHTML = `Paikkakunta: ${user.location}`;
-    area.classList.add("cardLocation");
+    area.classList.add("profArea");
 
     card.appendChild(img);
+    card.appendChild(heading)
     card.appendChild(name);
     card.appendChild(puh);
     card.appendChild(email);
